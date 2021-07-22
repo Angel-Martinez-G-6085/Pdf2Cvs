8fb5c60a489c0044fc15c3acceabb00bf1c1bfbc
Martinez Garrido Angel Ivan <mgangelivan@gmail.com>
2021-07-22 14:44:19 -0500

    se agrego la variable page al constructor y se implemento su uso en el resto de metodos

--- file/file.py
+++ file/file.py
@@ -1,10 +1,11 @@
 import PyPDF2
 class File:
     # cada que un objeto de tipo objeto es creado se solicita la ruta del archivo
-    def __init__(self,path):
+    def __init__(self,path,page):
         self.path=path
+        self.page=page
         
-    def extract(self,page):
+    def extract(self):
         """Obtiene el texto de una pagina especificada
 
         Args:
@@ -14,6 +15,6 @@ class File:
             objeto: contiene todo el texto de la pagina
         """        
         pdf=PyPDF2.PdfFileReader(self.path)
-        page_one_object=pdf.getPage(page)
+        page_one_object=pdf.getPage(self.page)
         page_one_text=page_one_object.extractText()
         return page_one_text
