2150f1bf229fa82bb607a09d74db386648357ac9
Martinez Garrido Angel Ivan <mgangelivan@gmail.com>
2021-07-22 20:12:03 -0500

    se reubico el archivo generado

--- file/file.py
+++ file/file.py
@@ -21,6 +21,6 @@ class File:
     
     def write_file(self):
         text=self._extract()
-        with open("file/texto.txt","w",encoding="utf-8") as file:
+        with open("data/texto.txt","w",encoding="utf-8") as file:
             for line in text:
                 file.write(line)
