fbfee446820a9aedad4f4911ec776c50567b9ef9
Martinez Garrido Angel Ivan <mgangelivan@gmail.com>
2021-08-17 19:14:22 -0500

    el script ya ouede poner todo el texto del PDF en un archivo txt

--- file/file.py
+++ file/file.py
@@ -1,26 +1,32 @@
 import PyPDF2
 class File:
-    # cada que un objeto de tipo objeto es creado se solicita la ruta del archivo
-    def __init__(self,path,page):
-        self.path=path
-        self.page=page
-        
-    def _extract(self):
-        """Obtiene el texto de una pagina especificada
+    """Obtiene el texto de una pagina especificada
 
         Args:
             page (Number): El numero de la pagina a obtener
 
         Returns:
             objeto: contiene todo el texto de la pagina
-        """        
+        """
+        
+    def __init__(self,path):
+        self.path=path
+    
+    
+    def _extract(self):        
         pdf=PyPDF2.PdfFileReader(self.path)
-        page_one_object=pdf.getPage(self.page)
-        page_one_text=page_one_object.extractText()
-        return page_one_text
+        page_number=pdf.getNumPages()
+        page_content=''
+        for page in range(0,page_number):
+            page_one_object=pdf.getPage(page)
+            page_one_text=page_one_object.extractText()
+            page_content+=page_one_text
+        return page_content
+    
     
     def write_file(self):
         text=self._extract()
+        texts=''
         with open("data/texto.txt","w",encoding="utf-8") as file:
             for line in text:
                 file.write(line)
