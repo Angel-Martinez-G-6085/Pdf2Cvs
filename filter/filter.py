c75044fb79c404615bf3a803fbe241b1b29055c3
Martinez Garrido Angel Ivan <mgangelivan@gmail.com>
2021-09-08 23:21:25 -0500

    se crea un objeto scanner en el cronstructor filter

--- filter/filter.py
+++ filter/filter.py
@@ -2,9 +2,13 @@ from scanner.scanner import Scanner
 
 class Filter:
     def __init__(self):
-        pass
+        self.scanner=Scanner()
     
     def get_General_Inf(self):
-        scanner=Scanner()
         campos=['Solicitud','Registrada como','Origen','Situación','Atendida como','Centro','INFORMACIÓN GENERAL','INFORMACIÓN DEL SERVICIO']
-        scanner.find('Solicitud',campos)
+        self.scanner.find('Solicitud',campos)
+    
+    
+    def getBitacora(self):
+        campos=['BITACORA DE MOVIMIENTOS','TRABAJOS REALIZADOS']
+        self.scanner.find('BITACORA DE MOVIMIENTOS',campos)
